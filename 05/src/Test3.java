7f49d3f3aa27dc599bc9dde7d9ff9ae9b63665e2
xuzefangfangtu <861421488@qq.com>
2018-11-24 09:10:20 +0800

    test

---
+++ 05/src/Test3.java
@@ -0,0 +1,38 @@
+//设计模式2：模块方法模式
+public class Test3 {
+    public static void main(String[] args) {
+        BaseManage bm = new UserManage();
+        bm.action("admin","add");
+        BaseManage bm2 = new TeacherManage();
+        bm2.action("admin","yes");
+    }
+}
+
+abstract class BaseManage{
+    public void action(String name,String method){
+        if("admin".equals(name)){
+            execute(method);
+        }
+    }
+    public abstract void execute(String method);
+}
+
+class UserManage extends BaseManage{
+    public void  execute(String method){
+        if("add".equals(method)){
+            System.out.println("++++++++++++++++++");
+        }else if("next".equals(method)){
+            System.out.println("------------------");
+        }
+    }
+}
+
+class TeacherManage extends BaseManage{
+    public void execute(String method){
+        if("yes".equals(method)){
+            System.out.println("yesyesyesyes");
+        }else if("no".equals(method)){
+            System.out.println("nononononono");
+        }
+    }
+}
